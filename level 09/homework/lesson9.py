ccd7b0c2a4d107e917a3831ca015f0d34c8d14cc
MaTeghv <ghviniashvilimate1@gmail.com>
2024-06-12 03:00:36 -0800

    commit

---
+++ level 09/homework/lesson9.py
@@ -0,0 +1,15 @@
+name = input('what is your name : ')
+
+last_name = input('what is your last name :')
+
+age = input('how old are you :')
+
+country = input('where are you from :')
+
+car = input('what is your favorite car :')
+
+color = input('what is your favorite color :')
+
+number = input('what is your favorite number :')
+
+print('my name is', name, last_name,  'i am',age, 'years old'  ' i am from',country,  'my favorite car is',car,  'my favorite color is',color,  'my favorite number is' ,number,)
