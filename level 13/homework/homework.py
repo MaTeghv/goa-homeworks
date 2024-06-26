a4adbb60d981db8c34ab37441c229178d69bc4ab
MaTeghv <ghviniashvilimate1@gmail.com>
2024-06-26 01:40:32 -0800

    commit

---
+++ level 13/homework/homework.py
@@ -0,0 +1,43 @@
+#1)
+
+
+a = int(input('Enter A: '))
+b = int(input('Enter B: '))
+c = int(input('Enter C: '))
+d = int(input('Enter D: '))
+
+
+print(a + b + c + d)
+print(a - b - c - d)
+print(a * b * c * d)
+print(a // b // c // d)
+ 
+ #2)
+
+mom_age = int(input('Enter your mom age: '))
+dad_age = int(input('Enter your dad age: '))
+sister_age = int(input('enter your sister age: '))
+ 
+print("my mom age after 20 years will be", mom_age + 20)
+print("my dad age after 20 years will be", dad_age + 20)
+print("my sister age after 20 years will be", sister_age + 20)
+
+
+#3)
+
+name = input('enter name: ')
+last_name = input('enter last_name: ')
+age = input('enter age: ')
+country = input('enter country: ')
+city = input('enter city: ')
+favorite_color = input('enter favorite_color: ')
+favorite_car = input('enter favorite_car: ')
+favorite_food = input('enter favorite_food: ')
+favorite_sport = input('enter favorite_sport: ')
+
+print("my name is", name ,last_name ,"I am ", age,"years old" "I live in", country, city, "my favorite_color is", favorite_color, "my favorite car is", favorite_car, "and my favorite food and sport is", favorite_food , favorite_sport)
+
+#4)
+
+name = input('enter name: ')
+print(name +' გილოცავ დაბადების დღეს მრავალს დაესწარი')
